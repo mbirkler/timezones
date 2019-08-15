ac02ba3cea4c6f1dd4e05c79caec210a05f4e917
mbirkler <m.birkler@knoll-mb.de>
2019-08-15 18:29:01 +0200

    Update de_DE.php

--- locales/de_DE.php
+++ locales/de_DE.php
@@ -1,1 +1,11 @@
-1
+<?php
+
+$LANG['timezones']['title']="Zeitzonen Verwalten";
+$LANG['timezones']['glpiversion']="This plugin requires GLPI >= 0.85";
+$LANG['timezones']['timezonetables']="mySQL 'time_zone' tables must be filled in before installing this plugin!<br>see: <a href='http://dev.mysql.com/doc/refman/5.6/en/time-zone-support.html#time-zone-installation'>Populating the Time Zone Tables</a>";
+$LANG['timezones']['dbnotconverted']="'dbupdate' script must be executed before enabling this plugin!<br>see: <a href='https://forge.indepnet.net/projects/timezones/wiki/installation' target='_blank'>See wiki</a>";
+
+$LANG['timezones']['item']['tab']="Zeitzone";
+$LANG['timezones']['item']['header']="Zeitzonen Verwaltung";
+$LANG['timezones']['item']['dropdown']="Zeitzone auswählen";
+$LANG['timezones']['item']['submit']="Speichern";
